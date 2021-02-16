e7a79e27a2e35d8602047c653a7c2bb4cb766539
snebotcifpfbmoll <snebot@cifpfbmoll.eu>
2021-02-16 15:33:24 +0100

    added main layout

---
+++ app/src/main/java/com/serafinebot/dint/game_2048/MainActivity.java
@@ -0,0 +1,28 @@
+package com.serafinebot.dint.game_2048;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_layout);
+    }
+
+    public void play(@NonNull View view) {
+        Intent intent = new Intent(this, GameActivity.class);
+        startActivity(intent);
+    }
+
+    public void scores(@NonNull View view) {
+        //Intent intent = new Intent(this, GameActivity.class);
+        //startActivity(intent);
+    }
+}
