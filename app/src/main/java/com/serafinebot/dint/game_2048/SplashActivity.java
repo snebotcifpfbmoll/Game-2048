3d3f8d12e23211ead714968fae5503be0cb182ad
snebotcifpfbmoll <snebot@cifpfbmoll.eu>
2021-02-25 12:52:56 +0100

    cleanup

--- app/src/main/java/com/serafinebot/dint/game_2048/SplashActivity.java
+++ app/src/main/java/com/serafinebot/dint/game_2048/SplashActivity.java
@@ -5,7 +5,6 @@ import android.animation.ValueAnimator;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.View;
 import android.view.ViewAnimationUtils;
 import android.view.animation.AccelerateInterpolator;
 import android.widget.TextView;
@@ -13,21 +12,9 @@ import android.widget.TextView;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class SplashActivity extends AppCompatActivity {
     private static final int SPLASH_TIME_OUT = 3000;
-    private static final Map<Integer, Integer> colorMap = new HashMap<>();
 
-    static {
-        colorMap.put(0, 0xFEF5F3);
-        colorMap.put(256, 0xf8b9a7);
-        colorMap.put(512, 0xf27c5b);
-        colorMap.put(1024, 0xeb4111);
-    }
-
-    private View contentView = null;
     private TextView counter = null;
     private ValueAnimator counterAnimator = null;
 
@@ -36,7 +23,6 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_layout);
 
-        this.contentView = findViewById(R.id.splash_container);
         this.counter = findViewById(R.id.counter_text);
 
         new Handler().postDelayed(() -> {
@@ -47,17 +33,6 @@ public class SplashActivity extends AppCompatActivity {
             finish();
         }, SPLASH_TIME_OUT);
         startAnimation();
-    }
-
-    private int getColorForValue(int value) {
-        int color;
-        int prev = 0;
-        for (int key : colorMap.keySet()) {
-            if (value < key) break;
-            prev = key;
-        }
-        color = colorMap.get(prev);
-        return color;
     }
 
     private void startAnimation() {
@@ -74,8 +49,6 @@ public class SplashActivity extends AppCompatActivity {
                 try {
                     int value = Integer.parseInt(animator.getAnimatedValue().toString());
                     this.counter.setText(String.valueOf(value));
-                    int color = getColorForValue(value);
-                    //this.counter.setTextColor(color);
                 } catch (Exception ignored) {
                 }
             });
